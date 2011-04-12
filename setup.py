53cfe28a58486ef1f97e7ca5927c2e959b42544c
Mike Yumatov <mike@yumatov.org>
2011-04-12 16:46:47 +0400

    Add classifiers

--- setup.py
+++ setup.py
@@ -9,6 +9,18 @@ setup(
     author = 'Mike Yumatov',
     author_email = 'mike@yumatov.org',
     packages = find_packages(),
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],
     entry_points = {
         'console_scripts': [
             'stencil = stencil.main:run',
