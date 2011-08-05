3ee01fd70ff70cea7ef002807d44afc3c29ef938
Mike Yumatov <mike@yumatov.org>
2011-08-05 17:55:36 +0400

    Remove unused function

--- stencil/main.py
+++ stencil/main.py
@@ -8,12 +8,6 @@ for entry_point in iter_entry_points('stencils'):
     stencils[entry_point.name] = entry_point.load()
 
 
-def print_list(option, opt, value, parser):
-    for name in sorted(stencils):
-        print(name)
-    sys.exit(0)
-
-
 def run():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--use-defaults', dest='use_defaults',
